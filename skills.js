9ce5ca36531fb947194c811704d4eb0961dcc6a1
fagarinis <47894627+fagarinis@users.noreply.github.com>
2024-01-10 22:15:20 +0000

    asd

--- skills.js
+++ skills.js
@@ -1,3 +1,16 @@
 function calculateNumbers(var1, var2) {
     console.log(var1 + var2);
 }
+
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
+}
